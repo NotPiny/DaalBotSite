cb35971744fdb3a0575b836cf8f1da79bd82dc9b
NotPiny <NotPiny@gmail.com>
2023-07-15 12:54:24 +0100

    Switch to new site design

---
+++ public/js/dashboard.js
@@ -0,0 +1,8 @@
+alert('This doesnt exist yet')
+window.location.href = '/'
+
+// if (localStorage.getItem('dashboard-token') === null) {
+//     window.location.href = '/Dashboard/Login'
+// } else {
+//     window.location.href = '/Dashboard/Select'
+// }
---
+++ public/js/home.js
@@ -0,0 +1,6 @@
+function onButtonClick(button) {
+    switch (button) {
+        case 'Github':
+            window.location.href = 'https://github.com/NotPiny/DaalBot'
+    }
+}
