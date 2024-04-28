5e5e4cc163c3936364f84af139aa3507a5cc21af
NotPiny <103984634+NotPiny@users.noreply.github.com>
2024-04-28 20:40:58 +0100

    Allow showing of access code instead of having to setup redirect route

--- src/routes/Dashboard/User/get-token/+page.svelte
+++ src/routes/Dashboard/User/get-token/+page.svelte
@@ -37,18 +37,33 @@
     async function returnData(allow) {
         if (!browser) return;
 
-        window.location.href = `${returnURL?.href}&code=${allow ? localStorage.getItem('accesscode') : 'denied'}`
+        if (returnURL?.href === 'https://daalbot.xyz/show') {
+            alert(localStorage.getItem('accesscode'));
+        } else {
+            window.location.href = `${returnURL?.href}&code=${allow ? localStorage.getItem('accesscode') : 'denied'}`
+        }
     }
 </script>
 
 <DevOnly>
     <main>
-        <h1>{returnName} is requesting access to manage DaalBot</h1>
-        <p>Do you want to grant access to this website?</p>
-        <p class="warning"><b>Granting access will allow "{hiddenName}" to act on your behalf and manage ALL of your servers</b></p>
+        {#if returnURL?.href === 'https://daalbot.xyz/show'}
+            <h1>Access Code</h1>
+            <p class="warning" style="text-align: center; margin-top: 0;"><b>
+                If you do not know what this is, do not share it with anyone. <br/>Your access code is a secret key that allows any application eg, This site to control DaalBot on your behalf.
+            </b></p>
+        {:else}
+            <h1>{returnName} is requesting access to manage DaalBot</h1>
+            <p>Do you want to grant access to this website?</p>
+            <p class="warning"><b>Granting access will allow "{hiddenName}" to act on your behalf and manage ALL of your servers</b></p>
+        {/if}
         <div class="control_buttons">
-            <button on:click={() => returnData(true)}>Grant Access</button>
-            <button on:click={() => returnData(false)}>Deny Access</button>
+            <button on:click={() => returnData(true)}>
+                {returnURL?.href === 'https://daalbot.xyz/show' ? 'Show Access Code' : 'Grant Access'}
+            </button>
+            {#if returnURL?.href !== 'https://daalbot.xyz/show'}
+                <button on:click={() => returnData(false)}>Deny Access</button>
+            {/if}
         </div>
         <p class="small"><a href="https://docs.daalbot.xyz/API/auth/learn">Learn More</a></p>
     </main>
